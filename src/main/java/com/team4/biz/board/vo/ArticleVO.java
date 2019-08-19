f9f3ba9662e0542332a6ba7c894e12b06b676ab4
shindong <swc00057@naver.com>
2019-08-19 21:13:22 +0900

    커밋2

---
+++ src/main/java/com/team4/biz/board/vo/ArticleVO.java
@@ -0,0 +1,96 @@
+package com.team4.biz.board.vo;
+
+import java.util.Date;
+
+public class ArticleVO {
+    private int id;
+    private int board_id;
+    private String title;
+    private String writer;
+    private int up_cnt;
+    private int view_cnt;
+    private String content_id;
+    private Date date_created;
+    private String category;
+    private String visible;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getBoard_id() {
+        return board_id;
+    }
+
+    public void setBoard_id(int board_id) {
+        this.board_id = board_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public int getUp_cnt() {
+        return up_cnt;
+    }
+
+    public void setUp_cnt(int up_cnt) {
+        this.up_cnt = up_cnt;
+    }
+
+    public int getView_cnt() {
+        return view_cnt;
+    }
+
+    public void setView_cnt(int view_cnt) {
+        this.view_cnt = view_cnt;
+    }
+
+    public String getContent_id() {
+        return content_id;
+    }
+
+    public void setContent_id(String content_id) {
+        this.content_id = content_id;
+    }
+
+    public Date getDate_created() {
+        return date_created;
+    }
+
+    public void setDate_created(Date date_created) {
+        this.date_created = date_created;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getVisible() {
+        return visible;
+    }
+
+    public void setVisible(String visible) {
+        this.visible = visible;
+    }
+}
---
+++ src/main/java/com/team4/biz/board/vo/BoardVO.java
@@ -0,0 +1,22 @@
+package com.team4.biz.board.vo;
+
+public class BoardVO {
+    private int id;
+    private String boardname;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBoardname() {
+        return boardname;
+    }
+
+    public void setBoardname(String boardname) {
+        this.boardname = boardname;
+    }
+}
