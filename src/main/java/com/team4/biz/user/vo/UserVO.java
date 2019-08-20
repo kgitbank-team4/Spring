bac121876d4b5589d7773a8940505210c77bafce
KGITBANK <KGITBANK@DESKTOP-NHP1H2G>
2019-08-20 15:48:12 +0900

    commit_Test1

---
+++ src/main/java/com/team4/biz/user/vo/UserVO.java
@@ -0,0 +1,51 @@
+package com.team4.biz.user.vo;
+
+import java.util.Date;
+
+public class UserVO {
+
+	private int id;
+	private String username;
+	private String password;
+	private String nickname;
+	private String tel;
+	private Date date_created;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public Date getDate_created() {
+		return date_created;
+	}
+	public void setDate_created(Date date_created) {
+		this.date_created = date_created;
+	}
+	
+	
+}
